a92c4da1c55ce4902afc6c7f4e1ad5e6c36261be
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2024-02-09 20:43:16 +0600

    input section styling done

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Input from "./components/Input";
 function App() {
   return (
     <MyProvider>
-      <main className="h-screen border-2 border-red-500 max-w-[50rem] m-auto py-10">
-        <h1 className="text-2xl text-center p-4 bg-orange-300 font-semibold">
-          My Todo List App
-        </h1>
-        <Input />
+      <main className="flex flex-col items-center justify-center h-screen bg-slate-100">
+        <div className="max-w-[50rem] h-full my-4 rounded-md overflow-hidden">
+          <h1 className="text-2xl text-center p-4 bg-green-600 text-white font-semibold">
+            My Todo List App
+          </h1>
+          <Input />
+        </div>
       </main>
     </MyProvider>
   );
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,17 +2,26 @@ import React from "react";
 
 function Input() {
   return (
-    <section className="flex justify-between items-center gap-4 p-4">
-      <input className="w-full" type="text" />
-      <div className="flex gap-2">
-        <label htmlFor="">Priority: </label>
-        <select>
+    <section className="flex justify-between items-center gap-4 p-4 bg-white">
+      <input
+        className="w-full p-2 outline-none border-2 focus:border-green-600 rounded"
+        placeholder="Write todo list..."
+        type="text"
+      />
+      <div className="flex gap-2 justify-center items-center">
+        <label className="font-semibold" htmlFor="priority">Priority: </label>
+        <select
+          id="priority"
+          className="border-2 text-white p-2 mr-2 cursor-pointer bg-green-600"
+        >
           <option value="option1">Low</option>
           <option value="option2">Medium</option>
           <option value="option3">High</option>
         </select>
       </div>
-      <button>Add</button>
+      <button className="bg-black text-white px-4 py-2 rounded font-semibold transition-all duration-300 hover:bg-slate-800">
+        Add
+      </button>
     </section>
   );
 }
